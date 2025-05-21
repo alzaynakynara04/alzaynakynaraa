2cf370553641604cb3b09a3074c74a4535a82890
alzaynakynara04 <alzaynakynaraa@gmail.com>
2025-05-21 09:26:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Selesai coooy🦑🪼🦩")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    Belajar Bersama Kynara"
 )
