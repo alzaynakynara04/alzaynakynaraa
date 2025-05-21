3a9f6c66ca6694bd5fa3fb867db9e833ba6a73c5
alzaynakynara04 <alzaynakynaraa@gmail.com>
2025-05-21 09:32:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Selesai coooy🦑🪼🦩")
-st.write(
-    BelajarBersamaKynara"
+st.write("BelajarBersamaKynara")
+    
 )
 st.image("IMG_20250516_144431_216.webp", width= 200)
